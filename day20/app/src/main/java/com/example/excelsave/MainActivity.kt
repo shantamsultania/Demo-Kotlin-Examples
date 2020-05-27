0752e994ea37e5c461a861b84e69249df3fa4df9
shantam sultania <48251619+shantamsultania@users.noreply.github.com>
2020-05-27 07:02:53 +0530

    this is progress for 30 days of Kotlin

--- day20/app/src/main/java/com/example/excelsave/MainActivity.kt
+++ day20/app/src/main/java/com/example/excelsave/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.excelsave
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
@@ -10,16 +11,20 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.opencsv.CSVWriter
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
 import java.io.File
 import java.io.FileWriter
+import java.io.InputStreamReader
 import java.lang.Exception
+import com.example.excelsave.R.raw.testing as testing1
 
 class MainActivity : AppCompatActivity() {
 
+
+    @SuppressLint("SdCardPath")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         save_btn.setOnClickListener {
 
@@ -32,14 +37,14 @@ class MainActivity : AppCompatActivity() {
             {
              try
              {
-                 var name = text1.text.toString()
-                 var lastname = text2.text.toString()
-                 var file = File("/sdcard/newfolder/")
+                 val name = text1.text.toString()
+                 val lastname = text2.text.toString()
+                 val file = File("/sdcard/newfolder/")
                  file.mkdirs()
 
-                 var csv = "/sdcard/newfolder/excel.csv"
-                 var csvwritter = CSVWriter(FileWriter(csv,true))
-                 var row= arrayOf(name,lastname)
+                 val csv = "/sdcard/newfolder/excel.csv"
+                 val csvwritter = CSVWriter(FileWriter(csv,true))
+                 val row= arrayOf(name,lastname)
                  csvwritter.writeNext(row)
                  csvwritter.close()
                  Toast.makeText(this,"done",Toast.LENGTH_SHORT).show()
@@ -51,6 +56,37 @@ class MainActivity : AppCompatActivity() {
                  e.printStackTrace()
              }
             }
+        }
+
+        var inputStream = resources.openRawResource(R.raw.testing)
+        var br = BufferedReader(InputStreamReader(inputStream))
+        var csvLine = br.readLine()
+        var data:List<String>
+        show_data_btn.setOnClickListener {
+
+            try{
+
+                while ((csvLine) != null)
+                {
+
+                    try {
+                        data = csvLine.split(",")
+                        csvLine = br.readLine()
+                        Log.e("excel ", data[0])
+
+                    }catch (e2:Exception)
+                    {
+                        Log.e("excel","$e2")
+                    }
+
+                }
+            }catch (e1:Exception)
+            {
+                Log.e("excel","$e1")
+            }
+
+
+
         }
 
     }
